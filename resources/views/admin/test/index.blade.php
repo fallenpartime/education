f0d1b7ce595c94a781fe261b7f331dc7ae0e81a8
fallen <rogerk08@126.com>
2018-10-08 20:27:08 +0800

    * 后台图片插件填充数据

--- resources/views/admin/test/index.blade.php
+++ resources/views/admin/test/index.blade.php
@@ -25,20 +25,9 @@
             key      : 'main_files'
         })
     </script>
+    @include('admin.layouts.picture')
     <script>
-        function initPicList(handle, id, name, srcUrl, thumbUrl, withDelete) {
-            $("#"+id).prepend('<div class="imguploader-doneitem" style="width: 100px;height: 100px">\n' +
-                '      <a class="fancybox" href="'+srcUrl+'" rel="group">\n' +
-                '        <img src="'+thumbUrl+'">\n' +
-                '      </a>\n' + (withDelete? '      <span class="imguploader-delbtn">×</span>\n': '') +
-                '      <span class="imguploader-progress" style="width: 100%; display: none;"></span>\n' +
-                '      <input name="'+name+'_preview[]" class="'+name+'_preview" value="'+srcUrl+'" type="hidden">\n' +
-                '      <input name="'+name+'_thumb[]" class="'+name+'_thumb" value="'+thumbUrl+'" type="hidden">\n' +
-                '    </div>')
-        }
-        alert(uploadhandle.imgNum)
-        initPicList(uploadhandle, 'container', 'main_files', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 1);
-        initPicList(uploadhandle, 'container', 'main_files', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 1);
-        uploadhandle.target.hidden();
+        initPictureList(uploadhandle, 'container', 'main_files', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 1);
+        // initPictureList(uploadhandle, 'container', 'main_files', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 'http://static.huijiayi.com.cn/liwudao/upload/image/20180928/7869849425.jpg', 1);
     </script>
 @endsection
