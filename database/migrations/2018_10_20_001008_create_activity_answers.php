ae70d1816dced0e705dd48a05b9b14019575383e
fallen <rogerk08@126.com>
2018-10-20 11:21:52 +0800

    *

--- database/migrations/2018_10_20_001008_create_activity_answers.php
+++ database/migrations/2018_10_20_001008_create_activity_answers.php
@@ -18,7 +18,7 @@ class CreateActivityAnswers extends Migration
             $table->integer('activity_id')->default(0)->comment('活动ID');
             $table->integer('question_id')->default(0)->comment('问题ID');
             $table->tinyInteger('type')->default(0)->comment('类型 0-文字 1-图片');
-            $table->string('title')->default('')->comment('问题描述');
+            $table->string('title',100)->default('')->comment('问题描述');
             $table->string('source')->default('')->comment('资源地址');
             $table->timestamps();
             $table->softDeletes();
