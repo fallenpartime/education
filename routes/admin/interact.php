de3e579cb61d993b0dcb86e3ce0b839c2e001cca
fallen <rogerk08@126.com>
2018-10-22 04:16:54 +0800

    *

--- routes/admin/interact.php
+++ routes/admin/interact.php
@@ -1,5 +1,5 @@
 <?php
-// 网络投票活动
+// 用户意见
 Route::middleware(['web', 'admin.login.auth', 'admin.action.auth'])->group(function () {
     Route::get('/admin/interact/admonitions', [
         'uses' => '\App\Http\Admin\Controllers\Interact\AdmonitionController@index'
