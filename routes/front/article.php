8030242df092a161c51318772c0c3cae07687f1f
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-10-30 12:21:28 +0800

    *

--- routes/front/article.php
+++ routes/front/article.php
@@ -4,7 +4,7 @@ Route::middleware(['web'])->group(function () {
     Route::match(['get', 'post'], '/front/article/news', [
         'uses' => '\App\Http\Front\Controllers\Article\NewsController@index'
     ])->name('front.news');
-    Route::get('/front/article/news/{code?}', [
+    Route::get('/front/article/news/{code}', [
         'uses' => '\App\Http\Front\Controllers\Article\NewsController@info'
     ])->name('front.news.info');
 });
