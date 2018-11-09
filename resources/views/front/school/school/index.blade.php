1770ccd0dffebc0bdb749fba8e496aeac9a4957f
fallen <rogerk08@126.com>
2018-11-09 22:15:55 +0800

    *

--- resources/views/front/school/school/index.blade.php
+++ resources/views/front/school/school/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>学校搜索</title>
     <link rel="stylesheet" href="/assets/front/css/index.css">
     <link rel="stylesheet" type="text/css" href="/assets/front/css/shadow.css">
     <script src="/assets/javascripts/jquery-1.10.2.min.js" type="text/javascript"></script>
